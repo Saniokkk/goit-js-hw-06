37752e937964618b203e1ec0c7cf6a35a041bf0f
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-01-09 02:49:20 +0200

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const listCategory = document.querySelector('#categories');
+
+console.log(`Number of categories: ${listCategory.children.length}`);
+
+function addListCategoryAndAmountElem() {
+    const itemsCategory = listCategory.querySelectorAll('li.item');
+
+    itemsCategory.forEach(item => {
+
+        return console.log(`
+    Category: ${item.firstElementChild.textContent}
+    Elements: ${item.querySelector('ul').children.length}`);
+    });
+}
+addListCategoryAndAmountElem();
