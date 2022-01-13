6700b75230cf773e46e3fe55555c638b845ce5e9
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-01-13 19:26:52 +0200

    task-10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+
+
+
+
+const btnChangeColorRef = document.querySelector('button.change-color');
+const btnChangeColorClick = btnChangeColorRef.addEventListener('click', onTargetButtonClick);
+const bodyRef = document.querySelector('body');
+
+function onTargetButtonClick() {
+  const backgroundColorBody = bodyRef.style.backgroundColor = getRandomHexColor();
+  document.querySelector('span.color').textContent = backgroundColorBody;
+}
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnCreateRef = document.querySelector('button[data-create]')
+const btnDestroyRef = document.querySelector('button[data-destroy]')
+const boxes = document.querySelector('div#boxes')
+
+btnCreateRef.addEventListener('click',createBoxes)
+btnDestroyRef.addEventListener('click', destroyBoxes)
+
+let sizeBox = 20;
+function createBoxes() {
+  const boxesArr = [];
+  const amount = document.querySelector("#controls input").value;
+  
+  for (let i = 1; i <= amount; i += 1) {
+    const box = document.createElement('div'); 
+    
+    sizeBox += 10;
+    box.style.width = `${sizeBox}px`;
+    box.style.height = `${sizeBox}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesArr.push(box);
+  }
+  return boxes.append(...boxesArr);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+  sizeBox = 20;
+}
+
